3ffe433e5969d99ab2ec1249548e6e30d0cd1749
Nate Stedman <natesm@gmail.com>
2016-03-27 09:33:55 -0400

    Add gestureProducer.

---
+++ UIKitProducers/Gesture.swift
@@ -0,0 +1,33 @@
+// UIKitProducers
+// Written in 2016 by Nate Stedman <nate@natestedman.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and
+// related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+import ReactiveCocoa
+import UIKit
+import enum Result.NoError
+
+// MARK: - gestureProducer
+public protocol GestureSignalProvidingType
+{
+    func rac_gestureSignal() -> RACSignal!
+}
+
+extension UIGestureRecognizer: GestureSignalProvidingType {}
+
+extension GestureSignalProvidingType
+{
+    /// Returns a signal producer that sends the receiver when its gesture occurs.
+    public var gestureProducer: SignalProducer<Self, NoError>
+    {
+        return rac_gestureSignal().toSignalProducer()
+            .demoteErrors()
+            .map { $0 as? Self }
+            .ignoreNil()
+    }
+}
